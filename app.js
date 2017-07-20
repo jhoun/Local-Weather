f7feb02eb7012210609c3caa0656558b85469b1f
jhoun <jayhoun@gmail.com>
2017-07-19 17:00:59 -0700

    implemented changing to celcius button

--- app.js
+++ app.js
@@ -3,10 +3,20 @@ navigator.geolocation.getCurrentPosition(function(position) {
     url: `https://api.darksky.net/forecast/1b5534a4ca117006f93923cf9fe1b0fb/${position.coords.latitude},${position.coords.longitude}`,
     dataType: "jsonp",
     success:function(result){
-      console.log('result: ', result);
+      var degrees = "<span class='temp'>"+ result.currently.temperature + '</span>°' + "<span class='click text'>F</span>";
       $(".summary").text(result.currently.summary)
-      $(".degrees").text(result.currently.temperature + '°' )
+      $(".degrees").append(degrees)
+      $("body").on("click", ".click", function(event){
+        console.log('event: ', event);
+        var c = (result.currently.temperature - 32) * 5 / 9;
+        if (event.target.innerHTML === 'F'){
+          $(".temp").text(c);
+          $(".text").text("C");
+        } else {
+          $(".temp").text(result.currently.temperature);
+          $(".text").text("F");
+        }
+      })
     }
   })
 });
-
